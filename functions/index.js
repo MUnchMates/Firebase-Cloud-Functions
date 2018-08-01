03434f40be525f5927a50fe7a5e1cf353cd44457
Liam Fruzyna <liamfruzyna@gmail.com>
2018-08-01 18:58:41 -0500

    Actually doesn't send messages to sender, plus respects notifications

--- functions/index.js
+++ functions/index.js
@@ -3,37 +3,48 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.sendMessageNotification = functions.database.ref('USERS/{userID}/conversations/messageList/{senderID}/messages/{messageID}').onWrite((change, context) => {
-  var receiver = context.params.userID
-  var sender = context.params.senderID
-  if (receiver == sender) {
-    return 0;
+  var userID = context.params.userID
+  var mateID = context.params.senderID
+  var messageData = change.after.val()  
+  console.log(userID + ' from convo with ' + mateID)
+  
+  if(messageData == null) {
+    console.log('Bad message update')
+    return 0
+  }
+  if(messageData.sender_id == userID) {
+    console.log('Not sending to sender')
+    return 0
   }
 
-  var messageData = change.after.val();    
-  const getInstanceIdPromise = admin.database().ref(`USERS/${receiver}/instanceId`).once('value');
+  const getUserPromise = admin.database().ref(`USERS/${userID}`).once('value')
+  return Promise.all([getUserPromise]).then(results => {
+    const user = results[0].val()
 
-  return Promise.all([getInstanceIdPromise]).then(results => {
-    const instanceId = results[0].val();
-
-    const payload = {
-      data: {
-        title: messageData.name,
-        body: messageData.text,
-        sender: sender,
-        date: messageData.dateTime
-      },
-      notification: {
-        title: messageData.name,
-        body: messageData.text
+    if(user.emailNotifications) {
+      const payload = {
+        data: {
+          title: messageData.name,
+          body: messageData.text,
+          sender: messageData.sender_id,
+          date: messageData.dateTime
+        },
+        notification: {
+          title: messageData.name,
+          body: messageData.text
+        }
       }
-    };
 
-    admin.messaging().sendToDevice(instanceId, payload)
-      .then(function (response) {
-        console.log("Successfully sent message:", response);
-      })
-      .catch(function (error) {
-        console.log("Error sending message:", error);
-      });
-  });
-});
+      admin.messaging().sendToDevice(user.instanceId, payload)
+        .then(function (response) {
+          console.log('Successfully sent message:', response)
+        })
+        .catch(function (error) {
+          console.log('Error sending message:', error)
+        })
+    }
+    else {
+      console.log('Not sending, user is muted')
+    }
+  })
+})
